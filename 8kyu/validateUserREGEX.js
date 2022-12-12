4746c914141260bb7eec5e321fe6cca03656cf28
DohBae <bdoherty92@msn.com>
2022-12-12 15:22:02 -0500

    Setup question

---
+++ 8kyu/validateUserREGEX.js
@@ -0,0 +1,17 @@
+/*
+Write a simple regex to validate a username. Allowed characters are:
+
+lowercase letters,
+numbers,
+underscore
+Length should be between 4 and 16 characters (both included).
+*/
+
+function validateUsr(username) {
+  res =  //regex here/.test(username) 
+  return res
+}
+
+validateUsr('asddsa') //true
+validateUsr('Hasd_12assssssasasasasasaasasasasas') //false
+validateUsr('____') //true
