e5be2ef3cd3aa0633b0f96d63ede5c617a47bafd
DohBae <bdoherty92@msn.com>
2022-12-20 10:23:09 -0500

    Setup solution

---
+++ 5kyu/movingZerosToEnd.js
@@ -0,0 +1,14 @@
+/*
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+EXAMPLE:
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+*/
+
+function moveZeros(arr) {
+  // look through arr
+  // check if element is a 0
+  // if it is 0, move it to the end of the array
+}
+
+moveZeros([false,1,0,1,2,0,1,3,"a"])
