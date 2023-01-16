f804604ae39c0137a68b3b8e343f583af37a3806
DohBae <bdoherty92@msn.com>
2023-01-16 11:06:11 -0500

    Add other's solutions for future notes

--- 6kyu/whoLikesIt.js
+++ 6kyu/whoLikesIt.js
@@ -39,3 +39,39 @@ likes(["Jacob", "Alex"])
 likes(["Max", "John", "Mark"])
 likes(["Alex", "Jacob", "Mark", "Max"])
 likes(["54fDGsdfs", "90099skldfjklajflks", "klajfdkljadlkaj f", "aijfsdjfasljf", "aijfsdjfasljf", "aijfsdjfasljf", "aijfsdjfasljf", "aijfsdjfasljf", "aijfsdjfasljf", "aijfsdjfasljf", "aijfsdjfasljf"])
+
+/*
+Other CodeWars Solution using switch statements:
+
+function likes(names) {
+  switch(names.length){
+    case 0:
+      return `no one likes this`;
+    case 1: 
+      return `${names[0]} likes this`;
+    case 2: 
+      return `${names[0]} and ${names[1]} like this`;
+    case 3: 
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default: 
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
+
+Other Solution using regex:
+
+function likes (names) {
+  var templates = [
+    'no one likes this',
+    '{name} likes this',
+    '{name} and {name} like this',
+    '{name}, {name} and {name} like this',
+    '{name}, {name} and {n} others like this'
+  ];
+  var idx = Math.min(names.length, 4);
+  
+  return templates[idx].replace(/{name}|{n}/g, function (val) {
+    return val === '{name}' ? names.shift() : names.length;
+  });
+}
+*/
