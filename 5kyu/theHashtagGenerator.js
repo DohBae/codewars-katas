a93bb071329a2c04d27edb03f78ab1d646517cdf
DohBae <bdoherty92@msn.com>
2023-01-19 11:54:29 -0500

    Clean up code and remove console.logs

--- 5kyu/theHashtagGenerator.js
+++ 5kyu/theHashtagGenerator.js
@@ -15,41 +15,26 @@ Examples
 ""                                        =>  false
 */
 
-function generateHashtag (str) {
-  // Look at a string
-  // Each word separated by at least one space
-  // Add # in front of word(s)
-  
+function generateHashtag(str) {
   // Remove spaces between words
   const newStr = str.replace(/\s/g, '')
-  // console.log(newStr)
-
-  // Capitalize the first letter
-  // const uppercaseStr = str.toUpperCase().replace(/\b[a-zA-Z0-9]/g)
-  // console.log(uppercaseStr)
-
-  const char = str.split(' ')
-  const wordArray = [];
 
   if (newStr.length > 140) {
-    console.log(false)
-    return false
-  }
+    return false;
+  };
 
   if (newStr.length === 0) {
-    console.log(false)
-    return false
-  }
+    return false;
+  };
 
-  const capitalizeChars = str.toLowerCase().split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join('')
+  const capitalizeChars = str.toLowerCase().split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join('');
 
-  console.log(`#${capitalizeChars}`)
-  return `#${capitalizeChars}`
-}
+  return `#${capitalizeChars}`;
+};
 
 generateHashtag("") //false
 generateHashtag(" ") //false
 generateHashtag("Hello world") //#HelloWorld
 generateHashtag("You Were My Brother Anakin") //#YouWereMyBrotherAnakin
 generateHashtag("you were    my brother anakin") //#YouWereMyBrotherAnakin
-generateHashtag("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat") //false
+generateHashtag("Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'") //false
