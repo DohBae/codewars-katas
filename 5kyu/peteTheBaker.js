27536e6a03fbfe614e05be9ffe91a1295ad64a8f
DohBae <bdoherty92@msn.com>
2023-01-20 13:19:04 -0500

    Add other answers for future refrence

--- 5kyu/peteTheBaker.js
+++ 5kyu/peteTheBaker.js
@@ -20,3 +20,16 @@ function cakes(recipe, available) {
 
 cakes({ flour: 500, sugar: 200, eggs: 1 }, { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }); //2
 cakes({ apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 }, { sugar: 500, flour: 2000, milk: 2000 }); //0
+
+/*
+Other Solution from Codewars:
+
+const cakes = (needs, has) => Math.min(
+  ...Object.keys(needs).map(key => Math.floor(has[key] / needs[key] || 0))
+)
+
+
+function cakes(recipe, available) {
+    return Math.min(...Object.keys(recipe).map(e => available[e]/recipe[e]>>0));
+}
+*/
