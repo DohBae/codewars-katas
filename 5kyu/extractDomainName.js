ba41028f9b7d4615ea870db8d419782da2c6dd5f
DohBae <bdoherty92@msn.com>
2023-01-20 16:02:11 -0500

    Add other solutions from codewars for future reference

--- 5kyu/extractDomainName.js
+++ 5kyu/extractDomainName.js
@@ -6,21 +6,10 @@ Write a function that when given a URL as a string, parses out just the domain n
 * url = "https://www.cnet.com"                -> domain name = cnet"
 */
 
-function domainName(url){
-  //Domain name is always before a period
-  //Domain name always follows a non-numeric character, either a . or /
-  // const regex = /(?<=\/\/|\.)(.*?)(?=\.)/
-  // // console.log(url.split(regex))
-  // const noWWW = url.replace(/(?:w{1,3}\.)/, '')
-  // console.log(noWWW)
-  // const noHttp = url.replace(/(?:http:\/\/)/, '')
-  // console.log(noHttp)
-  // const noHttps = url.replace(/(?:https:\/\/)/, '')
-  // console.log(noHttps)
-  const domName = url.replace(/(?:w{1,3}\.)/, '').replace(/(?:http:\/\/)/, '').replace(/(?:https:\/\/)/, '').split(/[/?#]/)[0]
-  console.log(domName.split([0]))
-  return domName.split([0])
-
+function domainName(url) {
+  const domName = url.replace('http://', '').replace('https://', '').replace('www.', '').split(/[/?#]/)[0];
+  const domain = domName.split(".")[0];
+  return domain;
 }
 
 domainName("http://www.google.com") //google
@@ -28,3 +17,11 @@ domainName("http://google.co.jp") //google
 domainName("www.xakep.ru") //xakep
 domainName("https://youtube.com") //youtube
 domainName('https://uet2ln2bazao6zhtudacw4sqw.edu/')
+
+/*
+Other Codewars Solution:
+
+function domainName(url){
+  return url.match(/(?:http(?:s)?:\/\/)?(?:w{3}\.)?([^\.]+)/i)[1];
+}
+*/
