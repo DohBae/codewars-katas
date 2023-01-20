420d958ac32f4f7a21361385b6a30cdab958a1c6
DohBae <bdoherty92@msn.com>
2023-01-20 10:16:44 -0500

    Set up kata answer

---
+++ 5kyu/rot13.js
@@ -0,0 +1,15 @@
+/*
+ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+
+Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+*/
+
+function rot13(message){
+  //Look at first letter in string
+  // From that letter, count to 13 
+  // Add 13th letter to new string
+
+}
+
+rot13("test") //grfg
+rot13("Test") //Grfg
