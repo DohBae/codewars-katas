be96cf325ddbbe8f2cfce2196893b976ab358363
DohBae <bdoherty92@msn.com>
2023-01-20 13:16:42 -0500

    Solve kata

--- 5kyu/peteTheBaker.js
+++ 5kyu/peteTheBaker.js
@@ -9,13 +9,25 @@ cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, mil
 cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}); 
 */
 
-function cakes(recipe, available) {
+// function cakes(recipe, available) {
   // Iterate through recipe object
   // Iterate through available object
   // Check to see if recipe and available have the same object keys
   // If they don't have the same keys, return 0
   // If they do have the same keys, divide available against recipe to get the number of cakes that key can make. Return the lowest quotient.
-}
+  
 
-cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200});
-cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}); 
+  function cakes(recipe, available) {
+    return Object.keys(recipe).reduce(function (el, ingredient) {
+      // console.log(available)
+      // console.log(available[ingredient])
+
+      return Math.min(
+        Math.floor(available[ingredient] / recipe[ingredient] || 0),
+        el
+      )
+    }, Infinity)
+  }
+
+cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); //2
+cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}); //0
